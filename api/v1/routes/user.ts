ec447bf8e9a2a9c07ca9152ac890d2da7f1b2fea
Shawn Stewart <shawn.stewart@quarkworks.co>
2020-06-01 10:36:57 -0500

    Hashing passwords and storing users in memory

--- api/v1/routes/user.ts
+++ api/v1/routes/user.ts
@@ -2,14 +2,28 @@ import { bcrypt, Router } from "../../../deps.ts";
 
 const router = new Router();
 
+interface User {
+  username: string;
+  password: string;
+}
+
+const users: User[] = [];
+
 router.get("/users", (ctx) => {
-  ctx.response.body = "users!";
+  ctx.response.body = { users };
 });
 
-router.post("/users/hash", async (ctx) => {
+router.post("/users/register", async (ctx) => {
   const body = await ctx.request.body();
+  const { username, password } = body.value;
 
-  ctx.response.body = { message: `going to hash ${body.value.password}` };
+  const hash = await bcrypt.hash(password);
+  const created = { username, password: hash };
+
+  users.push(created);
+
+  ctx.response.status = 201;
+  ctx.response.body = { message: `Created new user`, created };
 });
 
 export default router;
